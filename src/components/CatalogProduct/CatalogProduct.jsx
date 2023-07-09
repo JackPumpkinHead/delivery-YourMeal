bfc3c5a2c74b22ff0eb1e941b3e8f77bc2e2e0ce
JackPumpkinHead <kostyu444enko.igor@gmail.com>
2023-07-09 15:23:06 +0300

    feat: добавить логику работы корзины

--- src/components/CatalogProduct/CatalogProduct.jsx
+++ src/components/CatalogProduct/CatalogProduct.jsx
@@ -1,7 +1,11 @@
 import style from './CatalogProduct.module.css';
 import { API_URI } from '../../const.js';
+import { useDispatch } from 'react-redux';
+import { addProduct } from '../../store/order/orderSlice';
 
 export const CatalogProduct = ({ item }) => {
+    const dispatch = useDispatch();
+    
     return (
         <article className={style.product}>
             <img src={`${API_URI}/${item.image}`} alt={item.title} className={style.image} />
@@ -16,7 +20,11 @@ export const CatalogProduct = ({ item }) => {
 
             <p className={style.weight}>{item.weight}</p>
 
-            <button className={style.add} type="button">Добавить</button>
+            <button className={style.add} type="button" onClick={() => {
+                dispatch(addProduct({id: item.id}))
+            }}>
+                Добавить
+            </button>
         </article>
     )
 }
